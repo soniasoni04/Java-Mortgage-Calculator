780beb03b09aedb4d5678414249de14ccdeb7961
soniapathak <sonia.soni04@gmail.com>
2020-03-19 14:15:16 +0100

    implemented the mortgage formula

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,14 +14,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);;
 
         System.out.print("Enter the Principal Amount : ");
-        float p = scanner.nextFloat();
-        System.out.println("p : " + p);
+        float principal = scanner.nextFloat();
+        System.out.println("principal : " + principal);
         //
-        System.out.print("Enter the Annual Rate : ");
-        float r = scanner.nextFloat();
+        System.out.print("Enter the Annual Interest Rate : ");
+        float annualInterestRate = scanner.nextFloat();
+
+        float monthlyInterestRate = annualInterestRate/12*100;
         //
         System.out.print("Enter the Period of time in Years : ");
-        int n = scanner.nextInt();
+        int years = scanner.nextInt();
+        //
+        //now calculating the mortgage value
+        // M = p*r*Math.pow((1+r), n)/Math.pow((1+r), n) -1
+
+        double Mortgage = principal*monthlyInterestRate*Math.pow((1+monthlyInterestRate), years)
+                /Math.pow((1+monthlyInterestRate), years) -1;
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(Mortgage);
+        System.out.println("Mortgage value : " + mortgageFormatted);
+
 
 
 
